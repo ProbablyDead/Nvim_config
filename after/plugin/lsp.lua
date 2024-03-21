114a581e520623965dd7ec4ef01f4d1f92186638
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-03-21 06:09:18 +0300

    add lsps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,13 +5,13 @@ lsp_zero.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[g", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]g", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
@@ -20,6 +20,12 @@ end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
+      'cssls',
+      'dockerls',
+      'docker_compose_language_service',
+      'golangci_lint_ls',
+      'html',
+      'pylsp',
 	  'tsserver',
 	  'clangd',
 	  'unocss',
