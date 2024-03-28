c2310ee104510f2954f5205bec9e4019614b6c53
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-03-29 02:13:51 +0300

    add cloak and remove what i've added in the last commit

--- lua/my_nvim/packer.lua
+++ lua/my_nvim/packer.lua
@@ -6,7 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-    use 'j-hui/fidget.nvim'
+    use 'laytan/cloak.nvim'
 
 	use({
 		'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/Users/IlyaVolodin/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["cloak.nvim"] = {
+    loaded = true,
+    path = "/Users/IlyaVolodin/.local/share/nvim/site/pack/packer/start/cloak.nvim",
+    url = "https://github.com/laytan/cloak.nvim"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/Users/IlyaVolodin/.local/share/nvim/site/pack/packer/start/cmp-buffer",
@@ -103,11 +108,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/IlyaVolodin/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
-  },
-  ["fidget.nvim"] = {
-    loaded = true,
-    path = "/Users/IlyaVolodin/.local/share/nvim/site/pack/packer/start/fidget.nvim",
-    url = "https://github.com/j-hui/fidget.nvim"
   },
   ["friendly-snippets"] = {
     loaded = true,
